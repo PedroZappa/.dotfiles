3918e8b7996d7bc294202632eb62272377faffdf
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-17 20:12:14 +0000

    NixOS : getting flakes cleanup

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,12 +10,7 @@
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
