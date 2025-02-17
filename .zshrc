0143cb177497004c597a72163633aab2d36a4049
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-17 20:33:50 +0000

    NixOS : home-manager setting up

--- .zshrc
+++ .zshrc
@@ -92,7 +92,7 @@ zemoji() {
 #######################
 
 # NixOS
-alias nixre="sudo nixos-rebuild switch -I 'home=$HOME' --flake /etc/nixos#default"
+alias nixre="sudo nixos-rebuild switch --flake /etc/nixos"
 
 # Compiling
 alias ccw='cc -Wall -Wextra -Werror -g'
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, home ? "", ... }: 
+{ config, pkgs, inputs, ... }: 
 let
 # Import the unstable channel
   unstable = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
