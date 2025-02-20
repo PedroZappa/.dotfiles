5d980cce904d7ad6ef49c1dd37663a73c11b66ff
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-20 10:44:35 +0000

    nixos hostname for debug

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,7 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
     sha256 = "1blzcjd13srns4f5b4sl5ad2qqr8wh0p7pxbyl1c15lrsa075v8h";
   }) { system = system; };
-  hostname = "znix";
+  hostname = "nixos";
   user = "zedro";
 in
 {
