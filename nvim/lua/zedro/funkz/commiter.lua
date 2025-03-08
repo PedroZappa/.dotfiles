43867b1607b3c137c8c1e5a4f227519417b19c1a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-08 17:09:51 +0000

    tweak the prompt for commiter

--- nvim/lua/zedro/funkz/commiter.lua
+++ nvim/lua/zedro/funkz/commiter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.commit()
   vim.cmd(":Git add .")
   vim.cmd(":Git commit --verbose")
-  vim.cmd(":AvanteAsk 'Write a commit message describing the changes in this diff'")
+  vim.cmd(":AvanteAsk 'Write a Conventional Commit message describing the changes in this diff'")
 end
 
 return M
