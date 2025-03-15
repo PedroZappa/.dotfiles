7383435495c7297aa9bed3c591091b14bd190c96
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-15 12:39:04 +0000

    Update configuration and logs
    - Updated btop log with new session details.
    
    - Added a new keybind in hypr/config/keybinds.conf for opening terminal.
    
    - Enhanced hyprinit.sh script with environment variable exports for Hyprland.
    
    - Updated lazy-lock.json with new plugin commits.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- hypr/scripts/hyprinit.sh
+++ hypr/scripts/hyprinit.sh
@@ -11,3 +11,13 @@ nm-applet --indicator &
 eww &
 # Setup notifications
 swaync &
+
+# Send variables to the env
+export XDG_CURRENT_DESKTOP=Hyprland
+export XDG_SESSION_DESKTOP=Hyprland
+export XDG_SESSION_TYPE=wayland
+export WLR_NO_HARDWARE_CURSORS=1
+export LIBGL_ALWAYS_SOFTWARE=true
+export GALLIUM_DRIVER=llvmpipe
+
+exec Hyprland
