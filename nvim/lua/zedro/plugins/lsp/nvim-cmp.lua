989a6d649b2e95fa79a6387a9ee935d876a3069d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-03-16 21:02:29 +0000

    chore: update configuration and logs
    - Updated btop log with new runtime information and error logs.
    
    - Changed font in eww.scss to FiraCode Nerd Font.
    
    - Modified eww launch_bar configuration directory path.
    
    - Commented out waybar execution in hyprland.conf.
    
    - Updated lazy-lock.json to replace markdown.nvim with markview.nvim.
    
    - Commented out render-markdown.nvim plugin in avante.lua.
    
    - Added hrsh7th/cmp-nvim-lsp dependency in nvim-cmp.lua.
    
    - Commented out markdown.nvim plugin in markdown.lua.
    
    - Added new markview.lua plugin configuration.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- nvim/lua/zedro/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/zedro/plugins/lsp/nvim-cmp.lua
@@ -2,10 +2,11 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    'neovim/nvim-lspconfig',        -- LSP
+    "neovim/nvim-lspconfig",        -- LSP
+    'hrsh7th/cmp-nvim-lsp',
     "hrsh7th/cmp-buffer",           -- source for text in buffer
     "hrsh7th/cmp-path",             -- source for file system paths
-    'hrsh7th/cmp-cmdline',          -- source for cmdline
+    "hrsh7th/cmp-cmdline",          -- source for cmdline
     -- 'hrsh7th/cmp-omni',				-- source for omnifunc
     "L3MON4D3/LuaSnip",             -- snippet engine
     "saadparwaiz1/cmp_luasnip",     -- for autocompletion
@@ -13,7 +14,7 @@ return {
     "onsails/lspkind.nvim",         -- vs-code like pictograms
     -- { "quangnguyen30192/cmp-nvim-tags", ft = "markdown" },	-- nvim-tags
   },
-  config = function ()
+  config = function()
     -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require("cmp")
     local luasnip = require("luasnip")
@@ -51,7 +52,7 @@ return {
 
     cmp.setup({
       snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function (args)
+        expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
@@ -64,28 +65,28 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-2),
         ["<C-f>"] = cmp.mapping.scroll_docs(2),
-        ['<C-Space>'] = cmp.mapping(function ()
-          if (cmp.visible()) then
+        ["<C-Space>"] = cmp.mapping(function()
+          if cmp.visible() then
             cmp.abort()
           else
             cmp.complete()
           end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
         ["<C-x>"] = cmp.mapping.abort(), -- close completion window
         ["<C-y>"] = cmp.mapping.confirm({
           behaviour = cmp.ConfirmBehavior.Replace,
-          select = false
+          select = false,
         }),
-        ['<Tab>'] = cmp.mapping(function (fallback)
-          if (cmp.visible() and cmp.get_selected_entry() ~= nil) then
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() and cmp.get_selected_entry() ~= nil then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
           end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function (fallback)
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
@@ -93,7 +94,7 @@ return {
           else
             fallback()
           end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
       }),
       sources = cmp.config.sources({ -- sources for autocompletion
         { name = "nvim_lsp" },
@@ -105,7 +106,7 @@ return {
         { name = "codeium" }, -- codeium
         -- { name = "cmdline" }, -- cmdline
         {
-          name = 'tags',
+          name = "tags",
           option = {
             -- this is the default options, change them if you want.
             -- Delayed time after user input, in milliseconds.
@@ -132,42 +133,44 @@ return {
         completeopt = "menu,menuone,preview,noselect",
         -- autocomplete = false,
       },
-      formatting = { -- configure lspkind for vs-code like pictograms in completion menu
+      formatting = {
         fields = { "kind", "abbr", "menu" },
-        format = function (entry, vim_item)
-          -- Kind icons
-          -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-          vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        format = function(entry, vim_item)
+          -- Use your already defined kind_icons table
+          local kind = vim_item.kind
+          vim_item.kind = string.format("%s %s", kind_icons[kind] or "", kind)
+
+          -- Set menu based on source
           vim_item.menu = ({
-            nvim_lsp = "[lsp]",
-            nvim_lua = "[nvim_lua]",
-            lspkind  = "[lspkind]",
-            luasnip  = "[Snippet]",
-            buffer   = "[Buffer]",
-            path     = "[Path]",
-            codeium  = "[Codeium]",
-            cmdline  = "[Cmdline]",
-            tags     = "[Tags]",
-          })[entry.source.name]
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[Lua]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+            codeium = "[Codeium]",
+            cmdline = "[Cmd]",
+            tags = "[Tags]",
+          })[entry.source.name] or ""
+
           return vim_item
         end,
       },
     })
     -- `/` cmdline setup.
-    cmp.setup.cmdline('/', {
+    cmp.setup.cmdline("/", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = 'buffer' }
-      }
+        { name = "buffer" },
+      },
     })
     -- `:` cmdline setup.
-    cmp.setup.cmdline(':', {
+    cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'path' }
+        { name = "path" },
       }, {
-        { name = 'cmdline' }
-      })
+        { name = "cmdline" },
+      }),
     })
   end,
 }
