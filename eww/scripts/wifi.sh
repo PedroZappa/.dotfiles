a5d99db15928bb89b8b9dcf8e784ac450c3ccc76
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-05-11 17:13:53 +0100

    refactor(ui): update Wi-Fi widget and script
    Revised the Wi-Fi widget to remove the unused COL_WLAN variable and updated the script reference to wifi.sh. Adjusted the sidebar layout for better alignment and replaced horizontal orientation with vertical for improved readability. Simplified the Wi-Fi script by removing unused color output logic.
    
    Signed-off-by: Zedro <45104292+PedroZappa@users.noreply.github.com>

--- eww/scripts/wifi.sh
+++ eww/scripts/wifi.sh
@@ -16,16 +16,13 @@ essid=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
 if [ $status ] ; then
     icon="睊"        # Set icon to indicate disconnected state
     text=""          # No ESSID to display when disconnected
-    col="#575268"    # Set color for disconnected state
 else
     icon=""         # Set icon to indicate connected state (Wi-Fi symbol)
     text="${essid}"  # Display the ESSID of the connected network
-    col="#a1bdce"    # Set color for connected state
 fi
 
 # Output the requested value based on the first argument to the script
 case $1 in
-    --COL) echo $col;;     # Output the color code
-    --ESSID) echo $text;;  # Output the ESSID (network name)
     --ICON) echo $icon;;   # Output the icon
+    --ESSID) echo $text;;  # Output the ESSID (network name)
 esac
