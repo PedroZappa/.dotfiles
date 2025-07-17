0cf6f5e54f8a9a64baee57e5cb8eae74a8b88352
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-07-17 10:17:23 +0100

    Update dependencies, disable codecompanion.nvim, and configure obsidian.nvim
    - Updated multiple plugin dependencies with new commit hashes for
    stability and features.
    - Disabled `codecompanion.nvim` by commenting out its configuration due
    to inactivity or replacement.
    - Migrated to `obsidian-nvim/obsidian.nvim` (from
    `epwalsh/obsidian.nvim`) with dynamic vault path configuration and
    workspace adjustments for better Obsidian integration.

--- nvim/lua/zedro/plugins/obsidian.lua
+++ nvim/lua/zedro/plugins/obsidian.lua
@@ -1,32 +1,36 @@
+-- https://github.com/obsidian-nvim/obsidian.nvim?tab=readme-ov-file
+local vault_path = "~/Documents/Zedros-Vault"
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
+  -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  event = function()
+    return {
+      "BufReadPre " .. vim.fn.expand(vault_path) .. "/*.md",
+      "BufNewFile " .. vim.fn.expand(vault_path) .. "/*.md",
+    }
+  end,
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
 
-    -- see below for full list of optional dependencies 👇
+    -- see above for full list of optional dependencies ☝️
   },
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/Documents/Zedros-Vault",
+        name = "~/Documents/Zedros-Vault",
+        path = vault_path,
       },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
     },
 
     -- see below for full list of options 👇
